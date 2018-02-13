f6d662d70d30e83c893260e3a8d4989f5571449c
Nicolas DUBIEN <github@dubien.org>
2018-02-13 18:39:21 +0100

    Fix typo

--- test/async/conf.js
+++ test/async/conf.js
@@ -1,5 +1,5 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['todo-spec.js', 'todo-spec-2.js', 'todo-spec-3.js'],
-  SELENIUM_PROMISE_MANAGER: 1
+  SELENIUM_PROMISE_MANAGER: 0
 };
--- test/promise-manager/conf.js
+++ test/promise-manager/conf.js
@@ -1,5 +1,5 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['todo-spec.js'],
-  SELENIUM_PROMISE_MANAGER: 0
+  SELENIUM_PROMISE_MANAGER: 1
 };
