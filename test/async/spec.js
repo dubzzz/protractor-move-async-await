b477b89643170f8e6515e95a5261aff55917a0ea
Jayasankar <jayasankar.m@gmail.com>
2018-05-06 21:38:55 +0530

    Added workaround mentioned in the bug -- Javascript version :)

---
+++ test/async/spec.js
@@ -0,0 +1,20 @@
+let MakeSequential = function(func){
+    let prom = Promise.resolve();
+    return (...args) => {
+        prom = prom.then(function(){
+            return func(...args)
+        })
+        return prom
+    }
+}
+
+describe('[async] angularjs homepage todo list', function() {
+  it('should add a todo', async function() {
+    await browser.get('http://localhost:3000/index.html');
+    await element(by.model('todoList.todoText')).sendKeys('write first protractor test');
+    await element(by.css('[value="add"]')).click();
+    await element.all(by.repeater('todo in todoList.todos'))
+      .filter(MakeSequential(function(todo) {return todo.getText().then(label =>{console.log(label); return label.indexOf('#10') !== -1})}))
+      .each(MakeSequential(async function(todo,indx){ console.log(indx); await todo.element(by.css('input')).click()}));
+  });
+});
