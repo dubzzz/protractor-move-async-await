8daa65ecf681fcec7646ff6dbaf22b523b0ffa13
Jayasankar <jayasankar.m@gmail.com>
2018-05-06 21:47:25 +0530

    Update conf1.js

--- test/async/conf1.js
+++ test/async/conf1.js
@@ -1,7 +1,7 @@
 exports.config = {
   //directConnect:true,
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['todo-spec.js'],
+  specs: ['spec.js'],
   jasmineNodeOpts: {defaultTimeoutInterval: 500000},
   SELENIUM_PROMISE_MANAGER: 0
 };
